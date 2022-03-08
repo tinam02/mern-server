30deaf3f6d9f72477cdc86f5e6f6dc00e173d06a
tina <oge612@gmail.com>
2022-03-08 10:43:39 +0100

    add job, get all jobs (server side)

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -1,12 +1,25 @@
+import Job from "../models/Job.js";
+import { StatusCodes } from "http-status-codes";
+import { BadRequestError, UnAuthenticatedError } from "../errors/index.js";
+
 const createJob = async (req, res) => {
-  res.send("cccreate job");
+  const { position, company } = req.body;
+  if (!position || !company) {
+    throw new BadRequestError("Please provide job and company titles");
+  }
+  req.body.createdBy = req.user.userId;
+  const job = await Job.create(req.body);
+  res.status(StatusCodes.CREATED).json({ job });
 };
 const deleteJob = async (req, res) => {
   res.send("deleteJob job");
 };
 
 const getAllJobs = async (req, res) => {
-  res.send("getAllJobs job");
+  const jobs = await Job.find({ createdBy: req.user.userId });
+  res
+    .status(StatusCodes.OK)
+    .json({ jobs, totalJobs: jobs.length, numOfPages: 1 });
 };
 
 const updateJob = async (req, res) => {
